ee167b12d78cb20e4342a61ad745b61aae883e19
VicJopers <111643966+VicJopers@users.noreply.github.com>
2023-02-05 07:48:43 -0300

    Iniciando

---
+++ src/VicJopers/Projeto/Java/App.java
@@ -0,0 +1,9 @@
+package VicJopers.Projeto.Java;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo!");
+        System.out.println("Teste");
+    }
+}
